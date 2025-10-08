fb82c1ce89dfd705ac531fe6f848032e8a6724b4
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-10-08 13:51:14 -0500

    Migraciones de pacientes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            BloodTypeSeeder::class,
             RoleSeeder::class,
             UserSeeder::class
         ]);
