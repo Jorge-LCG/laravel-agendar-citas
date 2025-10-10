12a985def18cfe72b44e2bf143f3f3c5606cb124
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-10-10 00:01:03 -0500

    Guardar horario en a BD

--- app/Livewire/Admin/ScheduleManager.php
+++ app/Livewire/Admin/ScheduleManager.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin;
 
 use App\Models\Doctor;
+use App\Models\Schedule;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Livewire\Attributes\Computed;
@@ -64,7 +65,26 @@ class ScheduleManager extends Component
 
     public function save()
     {
-        dd($this->schedule);
+        $this->doctor->schedules()->delete();
+
+        foreach ($this->schedule as $day_of_week => $intervals) {
+            foreach ($intervals as $start_time => $isChecked) {
+                if ($isChecked) {
+                    Schedule::create([
+                        'doctor_id' => $this->doctor->id,
+                        'day_of_week' => $day_of_week,
+                        'start_time' => $start_time,
+                        'end_time' => Carbon::createFromTimeString($start_time)->addMinutes($this->apointment_duration)->format('H:i:s')
+                    ]);
+                }
+            }
+        }
+
+        $this->dispatch('swal', [
+            'icon' => 'success',
+            'title' => 'Horario actualizado',
+            'text' => 'El horario ha sido actualizado exitosamente'
+        ]);
     }
 
     public function render()
