c3df30d800704fb0ba5d0830d5c9fa505c4ccecf
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-10-10 01:51:44 -0500

    Migraciones para citas

---
+++ app/Models/Appointment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\AppointmentEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'date',
+        'start_time',
+        'end_time',
+        'duration',
+        'reason',
+        'status'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+        'status' => AppointmentEnum::class
+    ];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
